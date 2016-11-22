869685d40ee019e4eb88f30fd2f2149d9a596867
Spejderman <r.odgaard@gmail.com>
2016-11-22 11:59:59 +0100

    add initial server script

---
+++ TcpEchoServer/TcpEchoServer/TcpServer.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+
+
+namespace TcpEchoServer
+{
+	
+
+	public class TcpEchoServer
+	{
+
+		// Counting a's! 
+		public static void countFunction (object argument)
+		{
+			TcpClient client = (TcpClient)argument; 
+
+			try {
+
+				StreamWriter writer = new StreamWriter (client.GetStream (), Encoding.ASCII) { AutoFlush = true };
+				StreamReader reader = new StreamReader (client.GetStream (), Encoding.ASCII);
+
+				String inputLine = reader.ReadLine();
+
+				writer.WriteLine("i got: " + pik); 
+
+				reader.Close(); 
+				writer.Close(); 
+				client.Close(); 
+
+				Console.WriteLine("Connection with client closed..."); 
+			} 
+
+			catch (ThreadAbortException) {
+				Console.WriteLine ("Connection error...");
+				Thread.Sleep (2000); 
+			}
+
+			finally{
+				if (client != null) {
+					client.Close (); 
+				}
+			}
+		}
+
+		public static List<String> pik = new List<string>();
+
+		public static void Main ()
+		{
+			
+			Console.WriteLine ("Starting echo server...");
+
+			int port = 11000;
+			int nameId = 0;
+			IPAddress myIp = IPAddress.Parse ("192.168.0.14");
+
+			TcpListener listener = new TcpListener (myIp, port);
+			listener.Start ();
+
+			while (true) { 
+				TcpClient client = listener.AcceptTcpClient ();
+				Thread thread = new Thread (countFunction);
+				thread.Name = nameId.ToString();
+				thread.Start (client);
+
+				nameId++;
+
+			}
+		}
+	}
+}
+
