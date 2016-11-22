56160ed66f7e4988f9f516d721ded0339479ce6e
Spejderman <r.odgaard@gmail.com>
2016-11-22 14:13:32 +0100

    add server stuff

--- TcpEchoServer/TcpEchoServer/TcpServer.cs
+++ TcpEchoServer/TcpEchoServer/TcpServer.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Text;
 using System.Threading;
 using System.Collections.Generic;
-
+using System.Linq;
 
 
 namespace TcpEchoServer
@@ -22,19 +22,10 @@ namespace TcpEchoServer
 			TcpClient client = (TcpClient)argument; 
 
 			try {
+				StreamWriter writer = new StreamWriter(client.GetStream(), Encoding.ASCII) { AutoFlush = true };
+				StreamReader reader = new StreamReader(client.GetStream(), Encoding.ASCII);
 
-				StreamWriter writer = new StreamWriter (client.GetStream (), Encoding.ASCII) { AutoFlush = true };
-				StreamReader reader = new StreamReader (client.GetStream (), Encoding.ASCII);
-
-				String inputLine = reader.ReadLine();
-
-				writer.WriteLine("i got: " + pik); 
-
-				reader.Close(); 
-				writer.Close(); 
-				client.Close(); 
-
-				Console.WriteLine("Connection with client closed..."); 
+				Console.WriteLine("Trying to connect with client"); 
 			} 
 
 			catch (ThreadAbortException) {
@@ -43,13 +34,46 @@ namespace TcpEchoServer
 			}
 
 			finally{
-				if (client != null) {
-					client.Close (); 
+				Console.WriteLine("Client connected");
+				StreamWriter writer = new StreamWriter(client.GetStream(), Encoding.ASCII) { AutoFlush = true };
+				StreamReader reader = new StreamReader(client.GetStream(), Encoding.ASCII);
+
+				int messagesSend = 0;
+
+
+
+				messages.Add("her en gamle beskeder");
+				messages.Add("mere pis");
+
+				Console.WriteLine("now sending messages");
+				foreach (var message in messages){
+					messagesSend++;
+					writer.WriteLine("i got: " + message);
 				}
+
+				Console.WriteLine("Now listening to client");
+
+				while (true){
+					//Console.WriteLine("Recieved message: " + reader.ReadLine());
+					if (reader.ReadLine() != null) {
+						messages.Add(reader);
+						writer.WriteLine("i got: " + messages.ElementAt(messages.Count-1));
+					}
+
+					//if (messages.Count > messagesSend) {
+					//	writer.WriteLine("i got: " + messages.ElementAt(messagesSend));
+					//	messagesSend++;
+					//}
+
+				}
+				Console.WriteLine("Closing connection");
+				reader.Close();
+				writer.Close();
+				client.Close();
 			}
 		}
 
-		public static List<String> pik = new List<string>();
+		public static List<String> messages = new List<string>();
 
 		public static void Main ()
 		{
